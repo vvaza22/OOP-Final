9e1ae1c8d5b1c861adbf1fa0bb0dba6087166dfb
vvaza22 <vvaza22@freeuni.edu.ge>
2024-06-14 16:41:27 +0400

    added users database sql

---
+++ src/main/resources/users.sql
@@ -0,0 +1,14 @@
+use oop_final;
+
+drop table if exists users;
+
+create table users
+(
+    id            int auto_increment primary key,
+    user_name     varchar(16)  not null,
+    first_name    VARCHAR(32)  not null,
+    last_name     varchar(32)  not null,
+    password_hash varchar(256) not null,
+    image         varchar(256) null
+)
+    collate = utf8mb4_general_ci;
