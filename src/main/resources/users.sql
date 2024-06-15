9af4bec698ab73e1813ddbabacc2abd8be0a85f6
vvaza22 <vvaza22@freeuni.edu.ge>
2024-06-15 14:17:00 +0400

    added type column to users db

--- src/main/resources/users.sql
+++ src/main/resources/users.sql
@@ -5,10 +5,11 @@ drop table if exists users;
 create table users
 (
     id            int auto_increment primary key,
-    user_name     varchar(16)  not null,
+    user_name     varchar(16)  not null unique,
     first_name    VARCHAR(32)  not null,
     last_name     varchar(32)  not null,
     password_hash varchar(256) not null,
-    image         varchar(256) null
+    image         varchar(256) null,
+    type enum ('admin', 'user') not null
 )
     collate = utf8mb4_general_ci;
