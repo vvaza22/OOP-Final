9d7682085ada10a38b98f40ff85ac7dc9eca9e8c
vvaza22 <vvaza22@freeuni.edu.ge>
2024-07-03 01:04:38 +0400

    remove, move up/down functionality in quiz creator

--- src/main/webapp/js/create.js
+++ src/main/webapp/js/create.js
@@ -2,14 +2,17 @@
 
   // List of questions: State variable
   let questionList = [];
+  let uniqueIdCounter = 1;
 
-  function genFillBlank(questionNumber) {
+  function genFillBlank(uniqueId, questionNumber) {
     let container = document.createElement("div");
     container.className = "question-cont";
+    container.id = "question_" + uniqueId;
+    container.setAttribute("data-id", uniqueId);
     container.innerHTML =
         "<div class=\"question\">\n" +
         "    <div class=\"q-heading\">\n" +
-        "        <h5><b>Question #<span id=\"question_header_" + questionNumber + "\">" + questionNumber + "</span></b></h5>\n" +
+        "        <h5><b>Question #<span id=\"question_header_" + uniqueId + "\">" + questionNumber + "</span></b></h5>\n" +
         "        <span class=\"q-type-label\">Type: Fill in the blank</span>\n" +
         "    </div>\n" +
         "    <p><i>*Type {?} where the blank goes.</i></p>\n" +
@@ -20,22 +23,24 @@
         "    </div>\n" +
         "</div>\n" +
         "<div class=\"question-command\">\n" +
-        "    <button class=\"command-delete command-btn\"><i class=\"fa-solid fa-xmark\"></i></button>\n" +
+        "    <div onclick=\"removeQuestion(" + uniqueId + ")\" class=\"command-delete command-btn\"><i class=\"fa-solid fa-xmark\"></i></div>\n" +
         "    <div class=\"q-move\">\n" +
-        "        <button class=\"command-btn\"><i class=\"fa-solid fa-arrow-up\"></i></button>\n" +
-        "        <button class=\"command-btn\"><i class=\"fa-solid fa-arrow-down\"></i></button>\n" +
+        "        <div onclick=\"moveUp(" + uniqueId + ")\" class=\"command-btn\"><i class=\"fa-solid fa-arrow-up\"></i></div>\n" +
+        "        <div onclick=\"moveDown(" + uniqueId + ")\" class=\"command-btn\"><i class=\"fa-solid fa-arrow-down\"></i></div>\n" +
         "    </div>\n" +
         "</div>";
     return container;
   }
 
-  function genQuestionResponse(questionNumber) {
+  function genQuestionResponse(uniqueId, questionNumber) {
     let container = document.createElement("div");
     container.className = "question-cont";
+    container.id = "question_" + uniqueId;
+    container.setAttribute("data-id", uniqueId);
     container.innerHTML =
         "<div class=\"question\">\n" +
         "    <div class=\"q-heading\">\n" +
-        "        <h5><b>Question #<span id=\"question_header_" + questionNumber + "\">" + questionNumber + "</span></b></h5>\n" +
+        "        <h5><b>Question #<span id=\"question_header_" + uniqueId + "\">" + questionNumber + "</span></b></h5>\n" +
         "        <span class=\"q-type-label\">Type: Question-Response</span>\n" +
         "    </div>\n" +
         "    <textarea class=\"form-control\" placeholder=\"e.g. Who is the creator of the C Programming Language?\"></textarea>\n" +
@@ -45,19 +50,19 @@
         "    </div>\n" +
         "</div>\n" +
         "<div class=\"question-command\">\n" +
-        "    <button class=\"command-delete command-btn\"><i class=\"fa-solid fa-xmark\"></i></button>\n" +
+        "    <div onclick=\"removeQuestion(" + uniqueId + ")\" class=\"command-delete command-btn\"><i class=\"fa-solid fa-xmark\"></i></div>\n" +
         "    <div class=\"q-move\">\n" +
-        "        <button class=\"command-btn\"><i class=\"fa-solid fa-arrow-up\"></i></button>\n" +
-        "        <button class=\"command-btn\"><i class=\"fa-solid fa-arrow-down\"></i></button>\n" +
+        "        <div onclick=\"moveUp(" + uniqueId + ")\" class=\"command-btn\"><i class=\"fa-solid fa-arrow-up\"></i></div>\n" +
+        "        <div onclick=\"moveDown(" + uniqueId + ")\" class=\"command-btn\"><i class=\"fa-solid fa-arrow-down\"></i></div>\n" +
         "    </div>\n" +
         "</div>";
     return container;
   }
 
-  function appendQuestion(questionElem) {
+  function appendQuestion(uniqueId, questionElem) {
     const questionWrapper = document.getElementById("question-wrapper");
     questionWrapper.append(questionElem);
-    questionList.push(questionElem);
+    questionList.push(uniqueId);
   }
 
   function addQuestion(qType, qTypeText) {
@@ -65,11 +70,23 @@
     let questionNumber = questionList.length + 1;
     switch(qType) {
       case "q_type_1":
-        appendQuestion(genQuestionResponse(questionNumber));
+        appendQuestion(uniqueIdCounter, genQuestionResponse(uniqueIdCounter, questionNumber));
         break;
       case "q_type_2":
-        appendQuestion(genFillBlank(questionNumber));
+        appendQuestion(uniqueIdCounter, genFillBlank(uniqueIdCounter, questionNumber));
         break;
+    }
+    uniqueIdCounter++;
+  }
+
+  function reNumberQuestions() {
+    let questionContList = document.querySelectorAll("#question-wrapper > div");
+    let counter = 1;
+    for(let i=0; i<questionContList.length; i++) {
+      let questionCont = questionContList[i];
+      let uniqueId = parseInt(questionCont.getAttribute("data-id"));
+      document.getElementById("question_header_" + uniqueId).innerText = counter;
+      counter++;
     }
   }
 
@@ -82,6 +99,39 @@
       let selectedValue = questionType.value;
       let selectedIndex = questionType.selectedIndex;
       addQuestion(selectedValue, questionType.options[selectedIndex].text);
+    }
+
+    window.removeQuestion = function(questionId) {
+      const questionContainer = document.getElementById("question_" + questionId);
+      questionList = questionList.filter(function(elem) {
+        return elem !== questionId;
+      })
+      questionContainer.parentNode.removeChild(questionContainer);
+      reNumberQuestions();
+    }
+
+    window.moveUp = function(questionId) {
+      const questionContainer = document.getElementById("question_" + questionId);
+      let parentNode = questionContainer.parentNode;
+      let prevElement = questionContainer.previousElementSibling;
+
+      if(prevElement != null) {
+        parentNode.insertBefore(questionContainer, prevElement);
+      }
+
+      reNumberQuestions();
+    }
+
+    window.moveDown = function(questionId) {
+      const questionContainer = document.getElementById("question_" + questionId);
+      let parentNode = questionContainer.parentNode;
+      let nextNode = questionContainer.nextElementSibling;
+
+      if(nextNode != null) {
+        parentNode.insertBefore(nextNode, questionContainer);
+      }
+
+      reNumberQuestions();
     }
   }
 
