379c8baad3938793e65ce4064f478e0636fe33b9
ekvit22 <ekvit22@freeuni.edu.ge>
2024-07-05 12:54:46 +0400

    mail progress

--- src/main/java/Mail/FriendRequestMail.java
+++ src/main/java/Mail/FriendRequestMail.java
@@ -3,14 +3,20 @@ package Mail;
 import Account.Account;
 
 public class FriendRequestMail extends Mail {
+    private String status;
 
-    public FriendRequestMail(Account from, Account to) {
+    public FriendRequestMail(Account from, Account to, String status) {
         super(from, to);
+        this.status = status;
     }
 
     @Override
     public String getMessage() {
         return from.getUserName() + " sent you a friend request.";
+    }
+
+    public String getStatus(){
+        return status;
     }
 
     @Override
--- src/main/java/Mail/MailManager.java
+++ src/main/java/Mail/MailManager.java
@@ -1,6 +1,12 @@
 package Mail;
 
+import Account.Account;
 import Database.Database;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 public class MailManager {
     private final Database db;
@@ -9,5 +15,39 @@ public class MailManager {
         this.db = db;
     }
 
+    public ArrayList<Integer> getFriendRequests(int userId){
+        try{
+            Connection con = db.openConnection();
+            ArrayList<Integer> reqs = new ArrayList<>();
+            PreparedStatement stmt = con.prepareStatement(
+                    "select * from users where to_id = ?"
+            );
+            stmt.setInt(1,userId);
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()){
+//                Account from = new Account(
+//                        rs.get
+//                )
+//                FriendRequestMail req = new FriendRequestMail(
+//                        rs.getInt("from_id"),
+//                        rs.getInt("to_id"),
+//                        rs.getString("status")
+//                );
+
+
+
+            }
+
+
+
+
+            return reqs;
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 
 }
