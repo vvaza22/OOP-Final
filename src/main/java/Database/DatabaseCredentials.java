ec16ef7e33aad57a4423f876eb32edb8f9ea9c70
talkh22 <talkh22@freeuni.edu.ge>
2024-07-09 21:24:15 +0400

    added credentials file

---
+++ src/main/java/Database/DatabaseCredentials.java
@@ -0,0 +1,8 @@
+package Database;
+
+public class DatabaseCredentials {
+    public static final String DB_HOST = "127.0.0.1:3306";
+    public static final String DB_NAME = "oop_final";
+    public static final String DB_USER = "root";
+    public static final String DB_PASS = "tiafreeuni22";
+}
