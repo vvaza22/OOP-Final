f999c2954045948ef62c703766cfb55e8fadafee
ekvit22 <ekvit22@freeuni.edu.ge>
2024-07-09 16:34:29 +0400

    finished mail tests

--- src/test/java/Mail/ChallengeManagerTest.java
+++ src/test/java/Mail/ChallengeManagerTest.java
@@ -9,21 +9,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import Database.Database;
 import Database.DatabaseCredentials;
-import Mail.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-//These tests also test NoteMail and FriendRequestMail Classes
-//(makes use of ChallengeMail and ChallengeManager because of some methods)
+//These tests also test ChallengeMail (also makes use of MailManager)
 
-public class MailManagerTest extends TestCase {
-    private MailManager mailMgr;
-    private FriendRequestManager frmgr;
+public class ChallengeManagerTest extends TestCase {
     private ChallengeManager cmgr;
     private AccountManager amgr;
+    private MailManager mailMgr;
     private Database db;
     private Account acc1, acc2, acc3;
 
@@ -37,18 +34,15 @@ public class MailManagerTest extends TestCase {
         // Create Database Object
         db = new Database(dataSource);
         amgr = new AccountManager(db);
-        mailMgr = new MailManager(db, amgr);
-        frmgr = new FriendRequestManager(db);
         cmgr = new ChallengeManager(db);
+        mailMgr = new MailManager(db, amgr);
 
         try {
             Connection con = db.openConnection();
             Statement stmt = con.createStatement();
             stmt.executeUpdate("set FOREIGN_KEY_CHECKS=0"); // Disabling Foreign Key Checks
             stmt.executeUpdate("truncate table users");
-            stmt.executeUpdate("truncate table notes");
             stmt.executeUpdate("truncate table challenges");
-            stmt.executeUpdate("truncate table frreqs");
             stmt.executeUpdate("set FOREIGN_KEY_CHECKS=1"); // Enabling Foreign Key Checks
             stmt.close();
         } catch (SQLException e) {
@@ -93,62 +87,48 @@ public class MailManagerTest extends TestCase {
         amgr.registerAccount(acc3);
     }
 
-
     @Test
-    public void testNoteMail() throws SQLException {
-        int fromId = acc1.getUserId();
-        int toId = acc2.getUserId();
-        mailMgr.addNote(fromId, toId, "hello there");
-        mailMgr.addNote(fromId, toId, "hello again");
-        ArrayList<NoteMail> notes = mailMgr.getNotes(toId);
-        assertEquals(2, notes.size());
-        assertEquals(mailMgr.countNotes(toId), notes.size());
-        assertEquals("hello again", notes.get(1).getNote());
-        assertEquals(3, notes.get(0).getType());
-        assertEquals(amgr.getAccountById(fromId).getUserName() + " sent you a message.", notes.get(0).getMessage());
-        mailMgr.setNotesAsSeen(toId);
-    }
-
-    @Test
-    public void testFriendRequestMail() throws SQLException {
-        int fromId = acc2.getUserId();
-        int toId = acc1.getUserId();
-        frmgr.sendRequest(fromId, toId);
-        frmgr.sendRequest(acc3.getUserId(), toId);
-
-        ArrayList<FriendRequestMail> reqs = mailMgr.getFriendRequests(toId);
-        assertEquals(2, reqs.size());
-        assertEquals(mailMgr.countFriendRequests(toId), reqs.size());
-        assertEquals(1, reqs.get(0).getType());
-        assertEquals(1, reqs.get(0).getId());
-        assertEquals(acc3.getUserName() + " sent you a friend request.", reqs.get(1).getMessage());
-        assertEquals("PENDING", reqs.get(1).getStatus());
-        assertEquals(fromId, reqs.get(0).getFrom().getUserId());
-        assertEquals(toId, reqs.get(0).getTo().getUserId());
-    }
-
-    @Test
-    public void testGetChallenges() throws SQLException {
+    public void testSendChallenge() throws SQLException {
         int fromId = acc2.getUserId();
         int toId = acc1.getUserId();
         cmgr.sendChallenge(fromId, toId, 2);
         cmgr.sendChallenge(fromId, toId, 3);
         ArrayList<ChallengeMail> challenges = mailMgr.getChallenges(toId);
-        assertEquals(2, challenges.size());
-        assertEquals(mailMgr.countChallenges(toId), challenges.size());
+        assertEquals(2, challenges.get(0).getQuizId());
+        assertEquals(1, challenges.get(0).getId());
+        assertEquals(2, challenges.get(1).getType());
+
+        assertEquals(acc2.getUserName() + " challenged you with \"" + challenges.get(0).quiz_name + "\"",
+                challenges.get(0).getMessage());
+    }
+
+    @Test
+    public void testChangeStatus() throws SQLException {
+        int fromId = acc2.getUserId();
+        int toId = acc1.getUserId();
+        cmgr.sendChallenge(fromId, toId, 2);
+        cmgr.sendChallenge(fromId, toId, 1);
+        cmgr.sendChallenge(fromId, toId, 3);
+        ArrayList<ChallengeMail> challenges = mailMgr.getChallenges(toId);
+        assertEquals("PENDING", challenges.get(0).getStatus());
+        cmgr.changeStatus(challenges.get(0).getId(), "CHL_ACCEPTED");
+        cmgr.changeStatus(challenges.get(1).getId(), "CHL_REJECTED");
+        challenges = mailMgr.getChallenges(toId);
+        assertEquals("CHL_ACCEPTED", challenges.get(0).getStatus());
+        assertTrue(cmgr.challengeExists(fromId, toId, challenges.get(0).quiz_id));
+        assertTrue(cmgr.challengeExists(fromId, toId, challenges.get(2).quiz_id));
+        assertFalse(cmgr.challengeExists(fromId, toId, challenges.get(1).quiz_id));
     }
 
     @Test
     public void testWithRemovedUsers() throws SQLException {
         int fromId = acc2.getUserId();
         int toId = acc1.getUserId();
-        mailMgr.addNote(fromId, toId, "sorrow");
+        cmgr.sendChallenge(fromId, toId, 1);
         amgr.removeAccount(acc2.getUserName());
-        ArrayList<NoteMail> notes = mailMgr.getNotes(toId);
+        ArrayList<ChallengeMail> challenges = mailMgr.getChallenges(toId);
 
-        assertEquals("<b>[ deleted user ]</b> sent you a message",
-                notes.get(0).getMessage());
+        assertEquals("<b>[ deleted user ]</b> " + " challenged you with \""
+                + challenges.get(0).quiz_name + "\"", challenges.get(0).getMessage());
     }
-
-
 }
